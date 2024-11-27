931871a6c4f93aae436495ef6815ca375fd25a4d
rayhannr <rayhannr07@gmail.com>
2024-11-27 17:39:58 +0700

    feat: add music taste

--- api/index.ts
+++ api/index.ts
@@ -1,8 +1,34 @@
-const express = require('express')
+import express from 'express'
+import cors from 'cors'
+import OpenAI from 'openai'
+
 const app = express()
+const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY })
 
-app.get('/', (req, res) => res.send('Express on Vercel'))
+app.use(
+  cors({
+    origin: ['https://spoticeipt.vercel.app'],
+  })
+)
+app.get('/music-taste', async (req, res) => {
+  const { tracks } = req.query
+  if (!tracks) {
+    res.status(500).json({ message: 'you should add "tracks" query' })
+  }
+  try {
+    const parsedTracks = typeof tracks === 'string' ? [tracks] : (tracks as string[])
+    const content = `banter the music taste of someone whose top tracks is as follows:\n ${parsedTracks.join('\n')}`
+    const result = await openai.chat.completions.create({
+      messages: [{ role: 'user', content }],
+      model: 'gpt-4o-mini',
+      max_completion_tokens: 50,
+      temperature: 0.7,
+    })
 
-app.listen(3000, () => console.log('Server ready on port 3000.'))
+    res.status(200).json({ taste: result.choices[0].message.content || '' })
+  } catch (error) {
+    res.status(500).json({ error })
+  }
+})
 
 module.exports = app
